545d781184f42c315c3d18fe05e46c84e169af44
satohinash <206806064+satohinash@users.noreply.github.com.>
2025-10-31 11:36:56 +0900

    変更

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 // 自分で一から書いてみよう
+import { Hono } from 'jsr:@hono/hono';
+import { serveStatic } from 'jsr:@hono/hono/deno';
+const app = new Hono();
+
+app.use('/*', serveStatic({ root: './public' }));
+
+app.get('/api', async (c) => {
+    const res = await fetch('https://randomuser.me/api/')
+    const data = await res.json();
+
+    return c.json({ data });
+});
+
+Deno.serve(app.fetch);
